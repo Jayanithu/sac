4a8a4ce116f95016341eadcb048c54ed764db13c
Jayanithu <103057141+Jayanithu@users.noreply.github.com>
2025-11-16 23:50:02 +0530

    chore: update content paths to include app directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-=======
   content: ["./src/**/*.{js,ts,jsx,tsx}", "./app/**/*.{js,ts,jsx,tsx}"],
->>>>>>> dev
   theme: {
     extend: {},
   },

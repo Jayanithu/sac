020f2c365bb7e88772873f72fe725d7982e49014
Jayanithu <103057141+Jayanithu@users.noreply.github.com>
2025-11-23 00:01:22 +0530

    refactor: remove unused import React

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import CanvasSign from "../components/CanvasSign";
 import Preview from "../components/Preview";
 import ExportButtons from "../components/ExportButtons";

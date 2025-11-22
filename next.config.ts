220bf947d72d02cfb5af59d80e739848138223dd
Jayanithu <103057141+Jayanithu@users.noreply.github.com>
2025-11-23 02:11:14 +0530

    chore: remove turbopack config

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typedRoutes: false,
-  turbopack: {
-    root: "/Users/sithirajayanithu/Downloads/projects/sac"
-  }
 };
 
 export default nextConfig;
